e62ce6d9573ddba08e446c080db9dfff51fc57bd
AlekTananykin <tananykin@mail.ru>
2021-04-22 00:09:28 +0300

    GameObjectPool + PathMarks

--- Assets/Code/Controllers/PlayerController.cs
+++ Assets/Code/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Code.Exceptions;
 using Assets.Code.Interfaces;
 using Assets.Code.PlayerInput;
+using Assets.Code.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,15 @@ namespace Assets.Code.Controllers
     {
         private IPlayerInput _input;
         Camera _camera;
+        GameObjectsPool<PathMarksFabric> _pathMarkPool;
+        const uint _marksPoolSize = 5;
 
         internal PlayerController(IPlayerInput input, GameObject camera)
         {
             _input = input;
             _camera = camera.GetComponent<Camera>();
+            _pathMarkPool = new GameObjectsPool<PathMarksFabric>(
+                new PathMarksFabric(), _marksPoolSize);
         }
 
         public void Initialize()
@@ -37,19 +42,27 @@ namespace Assets.Code.Controllers
                 return;
 
             Ray ray = _camera.ScreenPointToRay(position);
-
-            Debug.Log(position);
-            Debug.DrawRay(ray.origin, ray.direction, Color.red);
-            
+ 
             if (!Physics.Raycast(ray, out RaycastHit hit))
                 return;
 
-            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            GameObject go = _pathMarkPool.Create();
             go.transform.position = hit.point;
+            PathMarkScript script = go.GetComponent<PathMarkScript>();
+            if (null == script)
+                throw new GameException("PathMarkScript is not attached. ");
+
+            script.OnPathMatkCall = OnPathMarkTrigger;
 
             Select_Point(hit.point);
         }
 
         public SelectPoint Select_Point;
+        public void OnPathMarkTrigger(GameObject pathObj)
+        {
+            PathMarkScript script = pathObj.GetComponent<PathMarkScript>();
+            script.OnPathMatkCall -= OnPathMarkTrigger;
+            _pathMarkPool.Intake(ref pathObj);
+        }
     }
 }
--- Assets/Code/Views/ViewsFabric.cs
+++ Assets/Code/Views/ViewsFabric.cs
@@ -1,4 +1,5 @@
 ﻿using Assets.Code.Exceptions;
+using Assets.Code.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using UnityEngine;
 
 namespace Assets.Code.Views
 {
-    class ViewsFabric
+    internal class ViewsFabric: GameObjectsLoader
     {
         private GameObject _hero;
         internal GameObject CreateHero()
@@ -19,7 +20,8 @@ namespace Assets.Code.Views
                     (GameObject)Resources.Load("Prefabs/Heroes/Hero");
                 if (null == heroPrefab)
                 {
-                    throw new GameException("CreateHero: Hero prefab is not found. ");
+                    throw new GameException(
+                        "CreateHero: Hero prefab is not found. ");
                 }
                 _hero = (GameObject)GameObject.Instantiate(heroPrefab);
             }
@@ -35,31 +37,12 @@ namespace Assets.Code.Views
                     (GameObject)Resources.Load("Prefabs/Player/Main Camera");
                 if (null == cameraPrefab)
                 {
-                    throw new GameException("CreateCamera: Camera prefab is not found. ");
+                    throw new GameException(
+                        "CreateCamera: Camera prefab is not found. ");
                 }
                 _camera = GameObject.Instantiate(cameraPrefab);
             }
             return _camera;
         }
-
-        private GameObject _pathMarkPrefab;
-        internal GameObject CreatePathMark()
-        {
-            return CreateObjectFromFile(
-                ref _pathMarkPrefab, "Prefabs/Heroes/PathMark");
-        }
-
-        GameObject CreateObjectFromFile(ref GameObject prefab, string prefabPath)
-        {
-            if (null == prefab)
-            {
-                prefab = (GameObject)Resources.Load(prefabPath);
-                if (null == prefab)
-                    throw new GameException("CreateObjectFromFile: " +
-                        "Prefab can't be loaded from file \"" + prefabPath + "\"");
-            }
-            return GameObject.Instantiate(prefab);
-        }
-
     }
 }
