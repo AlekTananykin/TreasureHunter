859ebd8aa2448815c5b592fbf3953f76535f034d
AlekTananykin <tananykin@mail.ru>
2021-05-05 23:10:49 +0300

    Modification

--- Assets/Code/Interfaces/IThing.cs
+++ Assets/Code/Interfaces/IThing.cs
@@ -10,6 +10,8 @@ namespace Assets.Code.Interfaces
     public interface IThing: ICloneable
     {
         LootName Name { get; set; }
+        int Cost { get; set; }
+        LootName Target { get; set; }
         IDictionary<LootProperties, int> Properties { get; }
         IDictionary<LootName, IThing> Components { get; }
     }
---
+++ Assets/Code/Things/ThingModifier.cs
@@ -0,0 +1,55 @@
+﻿using Assets.Code.Auxiliary;
+using Assets.Code.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code.Things
+{
+    internal static class ThingModifier
+    {
+        public static bool AddModification(
+            this IThing target, IThing modification)
+        {
+            if (target.Name != modification.Target ||
+                target.Components.ContainsKey(modification.Name))
+                return false;
+
+            var keys = modification.Properties.Keys;
+            foreach (LootProperties name in keys)
+            {
+                target.Properties[name] = target.Properties[name] + 
+                    modification.Properties[name];
+            }
+
+            target.Components.Add(modification.Name, modification);
+            return true;
+        }
+
+        public static IThing RemoveModification(
+            this IThing target, LootName modificationName)
+        {
+            if (!target.Components.TryGetValue(
+                modificationName, out IThing modification))
+                return null;
+
+            target.Components.Remove(modificationName);
+            var modificationKeys = modification.Properties.Keys;
+
+            foreach (LootProperties name in modificationKeys)
+            {
+                target.Properties[name] = target.Properties[name] -
+                    modification.Properties[name];
+            }
+            return modification;
+        }
+
+        public static IThing RemoveModification(
+           this IThing target, IThing modification)
+        {
+            return target.RemoveModification(modification.Name);
+        }
+    }
+}
